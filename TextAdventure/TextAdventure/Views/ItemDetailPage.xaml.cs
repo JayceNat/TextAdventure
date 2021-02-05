67ff0d23b1cb3fe458b046e395931f32bcc53e94
Jayce Meyer <jaycemeyer@gmail.com>
2021-02-04 20:28:31 -0700

    Initial project creation

---
+++ TextAdventure/TextAdventure/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using TextAdventure.ViewModels;
+using Xamarin.Forms;
+
+namespace TextAdventure.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ TextAdventure/TextAdventure/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using TextAdventure.Models;
+using TextAdventure.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TextAdventure.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
